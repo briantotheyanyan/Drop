a6ca7fcbd8acec958ea0b653bd7e41a8acf2c13b
Allen Wu <wu.allen154@gmail.com>
2013-01-12 15:56:16 -0500

    app.py, needtodo bugging. no login yet
    need to do bug testing. have not yet implemented login function

--- app.py
+++ app.py
@@ -4,30 +4,50 @@ import database
 
 app = Flask(__name__)
 #app.secret_key = "secret"
-name = ""
+mode = ""
 
 @app.route('/', methods=['GET', 'POST'])
-def check():
-	latitude=request.args.get('latitude','-1')
-	latitude=request.args.get('latitude','-1')
-#	database.returnMessagesinRange(longitude,latitude)
+def main():
+	global mode
+    	if request.method == 'GET':
+    		if mode = "SCAN":
+    			Latitude = request.form['Latitude']
+			Longitude = request.form['Longitude']
+    			messages = database.returnMessagesinRange(Latitude,Longitude)
+    			return render_template('GPS.html', messages = messages, mode = mode)
+    		
+		elif mode = "NEW":
+			return render_template('GPS.html',mode = mode)
+		else:
+    			return render_template('GPS.html',mode = mode)
+    	else:
+    		button = request.form['button']
+    		
+    		if button == 'BACK':
+			mode = ""
+			return redirect('/')
+		elif button == 'Create Message':
+			newM = request.form['title']
+			Latitude = request.form['Latitude']
+			Longitude = request.form['Longitude']
+			if newM:
+				writeMessage(newM,Longitude,Latitude):
+				mode = ""
+			return redirect('/')
+		elif button == 'Cancel':
+			mode = ""
+			return redirect('/')
+		elif button == 'SCAN':
+			mode = "SCAN"
+			return redirect('/')
+		elif button == 'NEW':
+			mode = "NEW"
+			return redirect('/')
+  		   
+    		
+    		
 
-@app.route("/write")
-def write():
-	latitude=request.args.get('latitude','-1')
-	latitude=request.args.get('latitude','-1')
-	text = request.form['text']
-	database.writeMessage(text,longitude,latitude):
-	return True
 
-@app.route("/register")
-def register():
-	username=request.args.get('username','')
-	password=request.args.get('password','')
-	birthday=request.args.get('birthday','')
-	database.createAccount(username,password,birthday):
-	 return "True"
-	
 if __name__=="__main__":
     app.debug=True
     app.run(host='0.0.0.0')
