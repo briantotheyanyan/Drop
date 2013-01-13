b879ccc95a0f038864260efc7467aa5a668642e2
Allen Wu <wu.allen154@gmail.com>
2013-01-12 21:25:05 -0500

    multipage, app.py, html not yet

--- app.py
+++ app.py
@@ -4,48 +4,81 @@ import database
 
 app = Flask(__name__)
 #app.secret_key = "secret"
-mode = ""
+
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	global mode
     	if request.method == 'GET':
-    		if mode == "SCAN":
+		return render_template('GPS.html',mode = mode)
+	else:
+    		button = request.form['button']
+#_______________________________________________________________Main page has "Scan" and "New" buttons
+
+    		if button == 'BACK':
+			mode = ""
+			return redirect('/')
+
+
+
+
+
+
+@app.route('/scan', methods=['GET', 'POST'])
+def scan():
+	global mode
+    	if request.method == 'GET':
+    		
     			Latitude = request.form['Latitude']
 			Longitude = request.form['Longitude']
     			messages = database.returnMessagesinRange(Latitude,Longitude)
     			return render_template('GPS.html',messages=messages, mode = mode)
-    		
-		elif mode == "NEW":
-			return render_template('GPS.html',mode = mode)
-		else:
-    			return render_template('GPS.html',mode = mode)
-    	else:
+	else:
     		button = request.form['button']
-    		
-    		if button == 'BACK':
-			mode = ""
+#_______________________________________________________________SCAN page has "Back" button
+		if button == button == 'SCAN':
+			mode = "SCAN"
 			return redirect('/')
-		elif button == 'Create Message':
-			newM = request.form['title']
+
+		elif button == 'NEW':
+			mode = "NEW"
+			return redirect('/')
+
+
+
+
+
+
+
+
+
+@app.route('/new', methods=['GET', 'POST'])  		   
+def new():    		
+       	global mode
+    	if request.method == 'GET':	
+		return render_template('GPS.html',mode = mode)
+	else:
+    		button = request.form['button']
+#_______________________________________________________________NEW page has "Create Message" and "Cancel" buttons
+		if button == 'Create Message':
+			newM = request.form['line']
 			Latitude = request.form['Latitude']
 			Longitude = request.form['Longitude']
 			if newM:
 				writeMessage(newM,Longitude,Latitude)
 				mode = ""
 			return redirect('/')
+
 		elif button == 'Cancel':
 			mode = ""
 			return redirect('/')
-		elif button == 'SCAN':
-			mode = "SCAN"
-			return redirect('/')
-		elif button == 'NEW':
-			mode = "NEW"
-			return redirect('/')
-  		   
-    		
-    		
+
+
+
+
 
 
 if __name__=="__main__":
