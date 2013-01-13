dc5d7a979f5ad3f7b7d7403f9bae7a584a681ec8
Allen Wu <wu.allen154@gmail.com>
2013-01-13 18:23:13 -0500

    export js

---
+++ static/scripts.js
@@ -0,0 +1,22 @@
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+function showPosition(position)
+  {
+document.getElementById("Latitude").value  =  position.coords.latitude;
+document.getElementById("Longitude").value =  position.coords.longitude;	
+
+ }
+
+
+
+$(document).ready(function() {
+
+       getLocation();
+})
+
