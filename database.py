0e58a65467d6c61b64af5bccd98a28e847d4ceb3
Brian Yan <sorakeybade@gmail.com>
2013-01-15 17:02:57 -0500

    Ok, changed the radius to 100 m

--- database.py
+++ database.py
@@ -32,7 +32,7 @@ def returnMessagesinRange(longitude,latitude):
     for current in allMessages:
         y=eval(str(current['longitude']))
         x=eval(str(current['latitude']))
-        if ((longitude-x)*(longitude-x))+((latitude-y)*(latitude-y)) <= 1:
+        if ((longitude-x)*(longitude-x))+((latitude-y)*(latitude-y)) <= 0.00089992800576:
             if messagesinRange == None:
                 messagesinRange = [str(current['text'].encode('ascii','ignore'))]
             else:
