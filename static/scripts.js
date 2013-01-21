af1650d41de677dd32e7bf3f02908d49a5de0d40
Shan Shi <a455898334@gmail.com>
2013-01-21 16:10:23 -0500

    gah

--- static/scripts.js
+++ static/scripts.js
@@ -1,9 +1,13 @@
-function getLocation(){
-	document.getElementById("Latitude").value  =  0;
-	document.getElementById("Longitude").value =  0;
-}
 function showPosition(position){
 	document.getElementById("Latitude").value  =  position.coords.latitude;
 	document.getElementById("Longitude").value =  position.coords.longitude;
 }
+
+if (navigator.geolocation){
+	navigator.geolocation.getCurrentPosition(showPosition);
+}else{
+	document.getElementById("Latitude").value  =  0;
+	document.getElementById("Longitude").value =  0;
+}
+
 
